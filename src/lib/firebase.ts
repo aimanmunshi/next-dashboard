5087bcf7f32464c9fb15db67a367ad4bd2b5e7a8
Aiman Munshi <munshiaiman2005@gmail.com>
2025-08-02 23:20:23 +0530

    protection

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { GithubAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSz_jgs2t6RJ4gOE4x1-JFo-nwGbUSzu8",
-  authDomain: "next-dashboard-app-2c496.firebaseapp.com",
-  projectId: "next-dashboard-app-2c496",
-  storageBucket: "next-dashboard-app-2c496.appspot.com",
-  messagingSenderId: "87894063895",
-  appId: "1:87894063895:web:659b6b0c476edb9c59d552",
-  measurementId: "G-HS27JEJCLK"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string,
 }
 
 // Prevent re-initialization in dev/hot reload
